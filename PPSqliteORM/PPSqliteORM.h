dc77a521f16e47a173063f23633c15e1aadf5750
许文发 <xuwenfa@star-net.cn>
2015-06-09 13:42:13 +0800

    Initial Commit

---
+++ PPSqliteORM/PPSqliteORM.h
@@ -0,0 +1,19 @@
+//
+//  PPSqliteORM.h
+//  PPSqliteORM
+//
+//  Created by StarNet on 6/9/15.
+//  Copyright (c) 2015 StarNet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PPSqliteORM.
+FOUNDATION_EXPORT double PPSqliteORMVersionNumber;
+
+//! Project version string for PPSqliteORM.
+FOUNDATION_EXPORT const unsigned char PPSqliteORMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PPSqliteORM/PublicHeader.h>
+
+
